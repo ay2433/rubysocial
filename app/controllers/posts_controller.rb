3c993b9dfd9e058608239114f5c8d54eb06e2666
ay2433 <a.y.ivanov707@gmail.com>
2021-12-14 18:00:45 +0600

    Change form update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,14 +8,33 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
+  def edit
+    @topics = Topic.all()
+    @post = Post.find(params[:id])
+  end
+
   def new
     @topics = Topic.all()
   end 
   
   def create
-    @post = Post.new(params.require(:post).permit(:title, :body, :topic_id))
+    @post = Post.new(post_params)
     @post.save
 
     redirect_to '/posts/' + @post.id.to_s
+  end 
+  
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+
+    redirect_to '/posts/' + @post.id.to_s
   end  
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :topic_id)
+  end
+
 end
